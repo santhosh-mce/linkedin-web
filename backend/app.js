4cad12bef4dc729ce81321db95d2a5151eabfdbf
Santhosh1507 <webdesign1507@gmail.com>
2024-09-11 22:48:50 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -13,17 +13,21 @@ import path from 'path';
 const app = express();
 dotenv.config()
 
-const __dirname = path.resolve()
+// const __dirname = path.resolve()
 
-if(process.env.NODE_ENV !== 'production') {
+// if(process.env.NODE_ENV !== 'production') {
+
+// }
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }))
-}
-
 app.use(express.json({limit: '5mb'}));
 app.use(cookieParser())
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
@@ -32,12 +36,12 @@ app.use('/api/v1/notifications', notificationRouter)
 app.use('/api/v1/connections', connectionRouter)
 connect();
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  })
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+//   })
+// }
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
