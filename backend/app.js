84fc45f25c6b6eee7694810f385f58c17db62d45
Santhosh1507 <webdesign1507@gmail.com>
2024-09-11 21:45:15 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,12 @@ dotenv.config()
 
 const __dirname = path.resolve()
 
+if(process.env.NODE_ENV !== 'production') {
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }))
+}
 
 app.use(express.json({limit: '5mb'}));
 app.use(cookieParser())
@@ -31,7 +33,7 @@ app.use('/api/v1/connections', connectionRouter)
 connect();
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   })
