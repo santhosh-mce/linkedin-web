836f483312de699235d98e96e22d597ef572bc1d
Santhosh1507 <webdesign1507@gmail.com>
2024-09-12 08:33:10 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 	baseURL: "http://localhost:3000/api/v1",
-	withCredentials: true,
 });
