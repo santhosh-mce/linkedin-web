1288cadf472405a1edea7044b0a87179462667bd
Santhosh1507 <webdesign1507@gmail.com>
2024-09-12 10:22:27 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,10 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
