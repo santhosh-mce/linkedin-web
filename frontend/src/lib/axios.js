7398251249f276bd3aa37040c94605b6102c6388
Santhosh1507 <webdesign1507@gmail.com>
2024-09-12 08:37:59 +0530

    first commit

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: "http://localhost:3000/api/v1",
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api/v1" : "/api/v1",
 });
